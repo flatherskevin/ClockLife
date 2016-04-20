d3c38f9d65dd74b7f804e7e6899aa310aa95d764
Kevin Flathers <flatherskevin@gmail.com>
2016-04-20 00:45:14 -0500

    Added simply analog clock

--- ClockLife.js
+++ ClockLife.js
@@ -24,13 +24,26 @@ function RotateMin(thisDiv){
 	angle = $(thisDiv).css('-webkit-transform', 'rotate(' + degrees + 'deg)');
 }
 
+function RotateHour(thisDiv){
+	curMin = new Date().getMinutes();
+	degrees = .25 * curMin;
+	angle = $(thisDiv).css('-webkit-transform', 'rotate(' + degrees + 'deg)');
+}
+
 //Intializes positions so that current time is the beginning rotation angle on page load
 $(document).ready(function(){
 	RotateSec('.minute_spinner');
 	RotateMin('.hour_spinner');
+	RotateSec('.second_analog');
+	RotateMin('.minute_analog');
+	RotateHour('.hour_analog');
 });
-//Object set for all objects within Spinner Clock
-Clock_Spinner = {
+
+///////////////////////////////////////////////////
+//Object set for all objects within Spinner Clock//
+///////////////////////////////////////////////////
+Clock_Spinner = 
+{
 	
 	//Assigns classes to display the hour of day, and inserts calculated hours in 12HR format
 	Hour:
@@ -70,12 +83,62 @@ Clock_Spinner = {
 	}
 }		
 
+//////////////////////////////////////////////////
+//Object set for all objects within Analog Clock//
+//////////////////////////////////////////////////
+Clock_Analog = 
+{
+	Hour:
+	{
+		classType: 'hour_analog',
+		handClass: 'hourHand_analog'
+	},
+	
+	Minute:
+	{
+		classType: 'minute_analog',
+		handClass: 'minuteHand_analog'
+	},
+	
+	Second:
+	{
+		classType: 'second_analog',
+		handClass: 'secondHand_analog'
+	},
+	
+	Pin:
+	{
+		classType: 'pin_analog'
+	},
+	
+	Twelve:
+	{
+		classType: 'twelve_analog'
+	},
+
+	Three:
+	{
+		classType: 'three_analog'
+	},
+
+	Six:
+	{
+		classType: 'six_analog'
+	},
+
+	Nine:
+	{
+		classType: 'nine_analog'
+	}
+}
+
 //Initialize clocks' app
 var Clock = angular.module('Clock',[]);
 
+//Main controller that takes care of async with html
 Clock.controller('MainController',function($scope, $interval){
 	$scope.timeSpinner = [Clock_Spinner.Hour,Clock_Spinner.DotDot,Clock_Spinner.Minute,Clock_Spinner.AmPm];
-	
+	$scope.timeAnalog = [Clock_Analog.Hour,Clock_Analog.Minute,Clock_Analog.Second,Clock_Analog.Pin,Clock_Analog.Twelve,Clock_Analog.Three,Clock_Analog.Six,Clock_Analog.Nine];
 	//Define intervals for clocks
 	$interval(function(){
 		
@@ -88,6 +151,9 @@ Clock.controller('MainController',function($scope, $interval){
 		$(document).ready(function(){
 			RotateSec('.minute_spinner');
 			RotateMin('.hour_spinner');
+			RotateSec('.second_analog');
+			RotateMin('.minute_analog');
+			RotateHour('.hour_analog');
 		});
 	}, 500);
 });
